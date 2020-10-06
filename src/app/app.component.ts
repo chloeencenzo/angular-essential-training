4914214d99f8ea9f353374052a51384ad93fc2b8
Chloe Sofia Encenzo <36587912+chloeencenzo@users.noreply.github.com>
2020-10-06 11:35:49 -0700

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'mw-app',
   templateUrl: './app.component.html',
-  styles: [` h1 { color: #ffffff; } `]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {}
