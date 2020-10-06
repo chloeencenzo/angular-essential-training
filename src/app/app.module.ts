41d1bf96067066a1ef8c6b111623cfeb5c223630
Chloe Sofia Encenzo <36587912+chloeencenzo@users.noreply.github.com>
2020-10-06 11:11:46 -0700

    Update app.module.ts

---
+++ src/app/app.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angularl/platform-browser';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  imports:      [ BroswerModule ],
+  declarations: [ AppComponent ],
+  bootstrap:    [ AppComponent ]
+})
+
+export class AppModule {}
